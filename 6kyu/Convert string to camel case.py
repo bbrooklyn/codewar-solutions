623870d8bd2f42d993558d8223e3cd7d1392b9c0
Brooklyn <58751447+bbrooklyn@users.noreply.github.com>
2022-03-21 10:11:26 +0000

    Update Convert string to camel case.py

--- 6kyu/Convert string to camel case.py
+++ 6kyu/Convert string to camel case.py
@@ -1,1 +1,9 @@
-#
+def to_camel_case(text):
+    sl = text.replace('-','_').split('_')
+    if text:
+        s = sl[0]
+        for a in sl[1:]:
+            s=s+a.capitalize()
+        return s
+    else:
+        return text
